b18265084d448d66e7935f53d537c7d7237e5be1
ABB22OllOst <olle.ostman@abbgymnasiet.se>
2022-10-24 12:18:17 +0200

    deleted light things in js

--- Temperatur.js
+++ Temperatur.js
@@ -16,33 +16,3 @@ firebase.initializeApp(firebaseConfig);
 
 const db = firebase.database();
 
-var ledRef = db.ref("led");
-
-ledRef.once("value").then((snapshot) => {
-  if (snapshot.val()) {
-    let myCheckBox = document.getElementById("checkbox");
-    myCheckBox.checked = snapshot.val().led;
-    if (myCheckBox.checked) {
-      document.getElementById("myImage").src = "light-bulb-on.png";
-    } else {
-      document.getElementById("myImage").src = "light-bulb-off.png";
-    }
-  }
-});
-
-ledRef.on("value", (snapshot) => {
-  const data = snapshot.val();
-});
-
-function turnOnOffLight() {
-  let myCheckBox = document.getElementById("checkbox");
-
-  if (myCheckBox.checked) {
-    document.getElementById("myImage").src = "light-bulb-on.png";
-    ledRef.update({ led: true });
-  } else {
-    document.getElementById("myImage").src = "light-bulb-off.png";
-    // database.ref("/led").update({ led: true });
-    ledRef.update({ led: false });
-  }
-}
